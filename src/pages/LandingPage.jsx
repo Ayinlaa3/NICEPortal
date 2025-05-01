3ffa59a8eae96a7726f3d6e358b7953fcdd658d5
Caleb <justcaleb01@gmail.com>
2025-05-01 12:08:44 +0100

    feat: add Publications component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import Banner from "../ui/landingpage/Banner.jsx";
 import Events from "../ui/landingpage/Events.jsx";
+import Journals from "../ui/landingpage/Journals.jsx";
 import Navbar from "../ui/landingpage/Navbar.jsx";
 import Ratings from "../ui/landingpage/Ratings.jsx";
 import WhoWeAre from "../ui/landingpage/WhoWeAre.jsx";
@@ -11,6 +12,7 @@ const LandingPage = () => {
       <WhoWeAre />
       <Ratings />
       <Events />
+      <Journals />
     </div>
   );
 };
